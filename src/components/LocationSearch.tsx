32831222663a31edcd1e35ee6ffc8ca9fe4e2b3e
lauren_m <laurenamolloy@gmail.com>
2025-11-24 10:23:48 +0200

    Adding helper function for API request

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -6,15 +6,25 @@ interface LocationSearchProps {
 }
 
 export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
-    const [places, setPlaces] = useState<Place[]>([])
+    const [places, setPlaces] = useState<Place[]>([]);
+    const [term, setTerm] = useState("");
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("Need to search", term);
+    }
+
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label className="font-bold" htmlFor="term">
                 Search
             </label>
             <input 
-            className="border border-gray-300 rounded-md shadow-sm foucs:border-indigo-500 px-4 py-2 w-full">
+            className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full"
+            id="term"
+            value={term}
+            onChange={e => setTerm(e.target.value)}>
             </input>
         </form>
     </div>
